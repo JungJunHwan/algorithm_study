fd1446012e1eb0d0f683d733d0373c375c8a0574
JungJunHwan <qoqkd0221@naver.com>
2024-12-24 12:20:58 +0900

    upload

--- src/Baekjoon/BOJ1700.java
+++ src/Baekjoon/BOJ1700.java
@@ -3,7 +3,7 @@ package Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BOJ1700 {
     public static void main(String[] args) throws IOException {
@@ -11,5 +11,27 @@ public class BOJ1700 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
+
+        int[] count = new int[100];
+        String[] input = br.readLine().split(" ");
+        for (int i = 0; i < k; i++) {
+            count[Integer.parseInt(input[i])-1]++;
+        }
+
+        int[] consent = new int[n];
+
+        
+
+
+        int consentCount = 0;
+
+
+
+        for (String a : input) {
+            if(consentCount < n) consentCount++;
+            else{
+
+            }
+        }
     }
 }
